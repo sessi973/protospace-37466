cd0476680088b16b81ad43ab48cc4bab5acfeb3c
sessi973 <hatano.seiko@gmail.com>
2022-02-25 04:08:25 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :prototypes
+  has_many :comments
+end
